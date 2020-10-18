b60bbd5464163a6021d73c26505302dabdf8e230
Sezoir <joshmottley0@gmail.com>
2020-10-18 22:58:35 +0100

    - Added Catch2 unit testing framework. - Setup basic testing architecture.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Engine
+A personal attempt at a game engine library.
+
+## Credits
+- Catch2: A multi-paradigm test framework for C++: https://github.com/catchorg/Catch2
---
+++ UnitTests/Window/WindowTests.cpp
@@ -0,0 +1,10 @@
+#include <catch.hpp>
+#include <Core/Application.hpp>
+
+TEST_CASE("Testing window", "[Window]")
+{
+    SECTION("Window creation")
+    {
+        Application app;
+    }
+}
