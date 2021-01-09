607a1811d120d9cb533da60f8eda760d46544345
Sezoir <joshmottley0@gmail.com>
2021-01-09 23:56:10 +0000

    - Added a header file which includes most general Rum library files. This is intended to be used for Rum applications.

---
+++ Rum/Rum.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// For use by Rum applications
+#include "Core/Application.hpp"
+
+#include "Core/Input.hpp"
+#include "Core/KeyCodes.hpp"
+#include "Core/MouseCodes.hpp"
+
+#include "Events/Event.hpp"
+#include "Events/KeyEvent.hpp"
+#include "Events/MouseEvent.hpp"
+
+// Useful typedefs
+typedef Rum::Core::Keyboard::Key Key;
+typedef Rum::Core::Mouse::Button Mouse;
